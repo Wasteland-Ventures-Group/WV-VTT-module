a4ee78373d12e6c7ccac2d8379678e2b678786bc
Kai Moschcau <mail@kmoschcau.de>
2020-12-20 14:19:34 +0100

    Make file names consistent and group them properly

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ const htmlHeader =
 <head>
   <title>Character Sheet</title>
   <link rel="stylesheet" href="../css/common.css">
+  <link rel="stylesheet" href="../css/actors/character-sheet.css">
 </head>
 `
 

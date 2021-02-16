068d5a0f8ca08fc6a5a7c602a6c9e621b0cbc9ab
Kai Moschcau <mail@kmoschcau.de>
2021-02-16 14:32:38 +0100

    Make the base data prep more functional

--- src/typescript/actor/wv-actor.ts
+++ src/typescript/actor/wv-actor.ts
@@ -4,16 +4,20 @@ import { PlayerCharacterData } from '../types/actor';
  * The basic Wasteland Ventures Actor.
  */
 export default class WvActor extends Actor<PlayerCharacterData> {
+  /**
+   * @override
+   */
   prepareBaseData(): void {
-    this._prepareBaseMaxHealth();
+    // TODO This needs to be changed. Somehow we need to add derived only data
+    // members to the Actor data without those bein saved to the database.
+    this.data.data.hitPoints = this.computeBaseMaxHealth();
   }
 
-  // = private base data preparation methods ===================================
-
   /**
-   * Set the max health in `data` from the values in `_data`.
+   * Compute the base max health, depending on the Endurance in
+   * {@link Entity._data}.
    */
-  _prepareBaseMaxHealth(): void {
-    this.data.data.hitPoints = this._data.data.endurance + 10;
+  protected computeBaseMaxHealth(): number {
+    return this._data.data.endurance + 10;
   }
 }

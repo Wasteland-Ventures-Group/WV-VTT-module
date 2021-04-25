17432b97f9256a318b25f66155c2d7e7ce5cc89a
Kai Moschcau <mail@kmoschcau.de>
2021-04-25 15:35:31 +0200

    Add registering the system Entity classes

--- src/typescript/wasteland-ventures.ts
+++ src/typescript/wasteland-ventures.ts
@@ -1,1 +1,8 @@
-Hooks.once("init", () => {});
+import WvActor from "./actor/wv-actor";
+import WvItem from "./item/wv-item";
+
+Hooks.once("init", () => {
+  // Register our own Entity classes.
+  CONFIG.Actor.entityClass = WvActor;
+  CONFIG.Item.entityClass = WvItem;
+});

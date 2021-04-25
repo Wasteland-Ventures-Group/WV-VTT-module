a988b865df1b6e93ac3d0621d650f57ad7230c9d
Kai Moschcau <mail@kmoschcau.de>
2021-04-25 17:34:05 +0200

    Add some documentation to constants.ts

--- src/typescript/constants.ts
+++ src/typescript/constants.ts
@@ -1,3 +1,10 @@
+/**
+ * This object holds common constants for the Wasteland Ventures system.
+ */
 export const CONSTANTS = {
+  /**
+   * This is the path for the system directory in Foundry's global working
+   * directory.
+   */
   systemPath: "systems/wasteland-ventures"
 };

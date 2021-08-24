015742ed723ea3eed77d05718e378112f141f9fd
Kai Moschcau <mail@kmoschcau.de>
2021-08-24 16:52:15 +0200

    Remove unused modded resource

--- src/typescript/data/common.ts
+++ src/typescript/data/common.ts
@@ -1,11 +1,3 @@
-import type * as foundry from "./foundryCommon.js";
-
-/** This is a {@link foundry.Resource} with an additional modifier. */
-export interface ModdedResource extends foundry.Resource {
-  /** The modifier value of a resource */
-  mod: number;
-}
-
 /** This represents an object that can be serialized to the `template.json`. */
 export interface TemplateDocumentType {
   /**

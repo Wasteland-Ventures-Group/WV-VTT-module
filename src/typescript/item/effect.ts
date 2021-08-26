12323f1f3a068e76346e966ca4d604e34a39a1af
Kai Moschcau <mail@kmoschcau.de>
2021-08-26 21:52:37 +0200

    Format comments in Effect

--- src/typescript/item/effect.ts
+++ src/typescript/item/effect.ts
@@ -3,13 +3,9 @@ import { TYPES } from "../constants.js";
 import RuleElements from "../ruleEngine/ruleElements.js";
 import WvItem from "./wvItem.js";
 
-/**
- * An Item that can represent an arbitrary effect, using RuleElements.
- */
+/** An Item that can represent an arbitrary effect, using RuleElements. */
 export default class Effect extends WvItem {
-  /**
-   * This constructor enforces that instances have the correct data type.
-   */
+  /** This constructor enforces that instances have the correct data type. */
   constructor(
     data: ConstructorParameters<typeof Item>[0],
     context: ConstructorParameters<typeof Item>[1]

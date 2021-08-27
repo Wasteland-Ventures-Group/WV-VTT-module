5fac9cd9b415a81b123f51d3f259c33e99e4f990
Kai Moschcau <mail@kmoschcau.de>
2021-08-28 00:09:00 +0200

    Add a lint-staged check for schemas and JSON data

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,6 @@
 module.exports = {
   "*.ts": "eslint --fix",
-  "*.(js|json)": "prettier --write"
+  "*.(js|json)": "prettier --write",
+  "src/schemas/lang.json": "ajv compile --strict true -s",
+  "src/compendiums/item/weapon/*.json": "ajv validate --strict true --errors text -s src/schemas/item/weapon.json -d"
 };

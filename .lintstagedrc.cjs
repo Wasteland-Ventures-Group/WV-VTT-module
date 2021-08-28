361727fc42e8896bb30b784a008909b3dc1f2d32
Kai Moschcau <mail@kmoschcau.de>
2021-08-28 09:52:58 +0200

    Add on-commit validation to lang files
    Closes #18

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -2,5 +2,6 @@ module.exports = {
   "*.ts": "eslint --fix",
   "*.(js|json)": "prettier --write",
   "src/schemas/lang.json": "ajv compile --strict true -s",
+  "src/lang/*.json": "ajv validate --strict true --errors text -s src/schemas/lang.json -d",
   "src/compendiums/item/weapon/*.json": "ajv validate --strict true --errors text -s src/schemas/item/weapon.json -d"
 };

1b3f113cedd13d1c4bb6de7a0f3642f96d5379d6
Kai Moschcau <mail@kmoschcau.de>
2021-08-28 09:48:55 +0200

    Add sort order to compendium entries and doc to schema

--- src/typescript/data/foundryCommon.ts
+++ src/typescript/data/foundryCommon.ts
@@ -13,14 +13,25 @@ export class Resource {
   ) {}
 }
 
+/** An interface to model Foundry's compendium layout. */
 export interface FoundryCompendiumData<T> {
-  _id: FoundryId;
+  /**
+   * The data base ID. It needs to be unique per compendium and should match the
+   * regular expression: `/^[a-zA-Z0-9]{16}$/`
+   */
+  _id: string;
+  /** The name of the entry in the compendium */
   name: string;
+  /** The Actor or Item type name */
   type: ValueOf<typeof TYPES.ACTOR | typeof TYPES.ITEM>;
+  /** The entry's system data */
   data: T;
+  /** The image of the entry */
   img: string;
+  /** Foundry Active Effects on the entry */
   effects: unknown[];
+  /** The sort order of the entry */
+  sort: number;
+  /** Custom flags on the entry */
   flags: Record<string, unknown>;
 }
-
-type FoundryId = string;

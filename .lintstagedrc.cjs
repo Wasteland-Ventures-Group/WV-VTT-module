be3aa907e7daf31882efcdcb72b7b5e2fa15539c
Kai Moschcau <mail@kmoschcau.de>
2021-09-14 21:06:18 +0200

    Refactor lint staged ajv command function

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -3,19 +3,18 @@ module.exports = {
   "*.(js|json)": "prettier --write",
   "src/schemas/lang.json": "ajv compile --strict true -s",
   "src/lang/*.json": (fileNames) =>
-    getAjvCommands(fileNames, "src/schemas/lang.json"),
+    getAjvCommand(fileNames, "src/schemas/lang.json"),
   "src/compendiums/item/weapon/*.json": (fileNames) =>
-    getAjvCommands(fileNames, "src/schemas/item/weapon.json")
+    getAjvCommand(fileNames, "src/schemas/item/weapon.json")
 };
 
 /**
- * Get the ajv commends for the provided file names and schema.
+ * Get the ajv command for the provided file names and schema.
  * @param {string[]} fileNames - the file names
  * @param {string} schema - the schema file name
  */
-function getAjvCommands(fileNames, schema) {
-  return fileNames.map(
-    (fileName) =>
-      `ajv validate --strict true --errors text -s ${schema} -d ${fileName}`
-  );
+function getAjvCommand(fileNames, schema) {
+  const fileOptions = fileNames.map((fileName) => `-d ${fileName}`).join(" ");
+
+  return `ajv validate --strict true --errors text -s ${schema} ${fileOptions}`;
 }

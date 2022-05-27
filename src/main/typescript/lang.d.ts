7dad97abbcded6d0d6ae3e145aba521481f2b558
Kai Moschcau <mail@kmoschcau.de>
2022-05-27 14:09:14 +0200

    Split up apparel sheet

--- src/main/typescript/lang.d.ts
+++ src/main/typescript/lang.d.ts
@@ -147,6 +147,8 @@ export interface LangSchema {
         slots: QuantityNames & {
           /** The name for the apparel slot concept */
           apparelSlot: string;
+          /** The names for blocked apparel slots */
+          blockedSlots: QuantityNames;
           /** The names for mod slots */
           modSlot: QuantityNames;
           /** The names for quick slots */

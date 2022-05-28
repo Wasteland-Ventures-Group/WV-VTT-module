27c378955c9a9ff8959ba91dea91c567a8a7ae3a
Sapphie <sapphie132@protonmail.com>
2022-05-28 20:51:34 +0200

    Implement MagicDataProperties stub

--- src/main/typescript/data/item/magic/properties.ts
+++ src/main/typescript/data/item/magic/properties.ts
@@ -1,6 +1,22 @@
 import type WvItem from "../../../item/wvItem.js";
-import { CompositeNumber } from "../../common.js";
 import RulesProperties from "../common/rules/properties.js";
-import StackableItemProperties from "../common/stackableItem/properties.js";
-import type AmmoDataSource from "./source.js";
+import type MagicDataSource from "./source.js";
+import BaseItemProperties from "../common/baseItem/properties.js";
 import { MagicDataSourceData } from "./source.js";
+
+export default interface MagicDataProperties extends MagicDataSource {
+  data: MagicDataSourceData;
+}
+
+export class MagicDataPropertiesData
+  extends MagicDataSourceData
+  implements BaseItemProperties
+{
+  constructor(source: MagicDataSourceData, owningItem: WvItem) {
+    super();
+    foundry.utils.mergeObject(this, source);
+    BaseItemProperties.transform(this, source, owningItem);
+  }
+
+  override rules = new RulesProperties();
+}

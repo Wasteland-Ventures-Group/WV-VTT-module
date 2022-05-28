b61f84471d08fe404f5f679b6c3e94514981547a
Sapphie <sapphie132@protonmail.com>
2022-05-28 22:09:02 +0200

    Add MagicSheet stub

--- src/main/typescript/applications/item/magicSheet.ts
+++ src/main/typescript/applications/item/magicSheet.ts
@@ -1,3 +1,16 @@
-import WvItemSheet from "./wvItemSheet";
+import WvItemSheet, { SheetData } from "./wvItemSheet";
+import type Magic from "../../item/magic";
 
-export default class MagicSheet extends WvItemSheet {}
+export default class MagicSheet extends WvItemSheet {
+  static override get defaultOptions(): ItemSheet.Options {
+    const defaultOptions = super.defaultOptions;
+    defaultOptions.classes.push("magic-sheet");
+    defaultOptions.height = 260;
+    defaultOptions.width = 670;
+    return defaultOptions;
+  }
+
+  override get item(): Magic {
+    return super.item;
+  }
+}

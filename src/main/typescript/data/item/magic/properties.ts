ff6e916482ac6061090852fae271487ebe485c98
Sapphie <sapphie132@protonmail.com>
2022-08-23 10:17:51 +0200

    Add properties types to magic

--- src/main/typescript/data/item/magic/properties.ts
+++ src/main/typescript/data/item/magic/properties.ts
@@ -1,9 +1,12 @@
 import { MagicType, getMagicType } from "../../../constants.js";
 import type WvItem from "../../../item/wvItem.js";
+import { CompositeNumber } from "../../common.js";
 import BaseItemProperties from "../common/baseItem/properties.js";
 import RulesProperties from "../common/rules/properties.js";
+import RangeProperties from "./ranges/properties.js";
 import type MagicDataSource from "./source.js";
 import { MagicDataSourceData } from "./source.js";
+import { TargetProperties } from "./target/target.js";
 
 export default interface MagicDataProperties extends MagicDataSource {
   data: MagicDataPropertiesData;
@@ -18,9 +21,23 @@ export class MagicDataPropertiesData
     foundry.utils.mergeObject(this, source);
     this.type = getMagicType(this.school);
     BaseItemProperties.transform(this, source, owningItem);
+
+    this.apCost = CompositeNumber.from(source.apCost);
+    this.strainCost = CompositeNumber.from(source.strainCost);
+
+    this.range = new RangeProperties(source.range);
+    this.target = new TargetProperties(source.target);
   }
 
   override rules = new RulesProperties();
+
+  override apCost: CompositeNumber;
+
+  override strainCost: CompositeNumber;
+
+  override range: RangeProperties;
+
+  override target: TargetProperties;
 
   type: MagicType;
 }

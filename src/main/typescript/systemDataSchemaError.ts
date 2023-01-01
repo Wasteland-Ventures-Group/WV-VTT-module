5353556c53c93ac96661cd090e05d1166d651e4e
Sapphie <sapphie132@protonmail.com>
2023-01-01 14:36:13 +0100

    Update docstring

--- src/main/typescript/systemDataSchemaError.ts
+++ src/main/typescript/systemDataSchemaError.ts
@@ -2,7 +2,7 @@ import type { ZodIssue } from "zod";
 
 /** A custom error to signal invalid system data */
 export default class SystemDataSchemaError extends Error {
-  /** @param errors - the AJV validation errors */
+  /** @param errors - the zod parsing issues */
   constructor(public errors: ZodIssue[]) {
     super("The system data was not valid.");
   }

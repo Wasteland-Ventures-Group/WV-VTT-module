532418236c9445aa49675bc8808c313abefc9f71
Sapphie <sapphie132@protonmail.com>
2023-01-01 14:34:00 +0100

    Format file

--- src/main/typescript/ruleEngine/ruleElementSource.ts
+++ src/main/typescript/ruleEngine/ruleElementSource.ts
@@ -25,22 +25,34 @@ export type RuleElementSource = z.infer<typeof RULE_ELEMENT_SCHEMA>;
 export const RULE_ELEMENT_SCHEMA = z.object({
   /** Whether this rule element is enabled */
   enabled: z.boolean(),
+
   /** Where in the data preparation chain the rule element applies */
   hook: z.enum(RULE_ELEMENT_HOOKS),
+
   /**
    * Optional conditions when this RuleElement should apply. All of the
    * conditions need to be met for the RuleElement to apply.
    */
   conditions: z.array(z.enum(RULE_ELEMENT_CONDITIONS)),
+
   /** The label of the element */
   label: z.string(),
+
   /** The place in the order of application, starting with lowest */
   priority: z.number(),
+
+  /**
+   * Optional conditions when this RuleElement should apply. All of the
+   * conditions need to be met for the RuleElement to apply.
+   */
   selectors: z.array(DOCUMENTSELECTOR_SCHEMA),
+
   /** The target property on the selected document */
   target: z.string(),
+
   /** The type identifier of the element. */
   type: z.enum(RULE_ELEMENT_IDS),
+
   /** The value of the element */
   value: z.union([z.boolean(), z.number(), z.string()])
 });

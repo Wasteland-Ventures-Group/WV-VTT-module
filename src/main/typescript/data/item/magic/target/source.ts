1847c438f4938531c26361d0dbcfa1b3a0d5186d
Sapphie <sapphie132@protonmail.com>
2023-01-01 14:46:21 +0100

    Add documentation

--- src/main/typescript/data/item/magic/target/source.ts
+++ src/main/typescript/data/item/magic/target/source.ts
@@ -6,9 +6,16 @@ export const AOETypes = ["none", "fixed", "varies"] as const;
 
 export const TARGET_SCHEMA = z
   .object({
+    /** Determines the type of target a spell can affect */
     type: z.enum(TargetTypes).default("none"),
+    /**
+     * Determines how many of `type` a spell can affect. Usually only applies
+     * when `type` is `creature`
+     */
     count: COMP_NUM_SCHEMA.default({}),
+    /** Determines if the area of effect can grow based on potency */
     aoeType: z.enum(AOETypes).default("none"),
+    /** Determines the size of the area of effect if static */
     fixedAoE: z.enum(SplashSizes).default("tiny")
   })
   .default({});
